01ffec53b5fe404e83850fcdf65cbe7cb743e3c9
Jay Butera <buterajay@gmail.com>
2016-12-19 19:10:54 -0600

    Add rendering manager

---
+++ src/main/java/DisplayManager.java
@@ -0,0 +1,72 @@
+import java.nio.IntBuffer;
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class DisplayManager {
+    // Configuration parameters
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = WIDTH / 2;
+
+    private static GLFWErrorCallback errorCallback
+            = GLFWErrorCallback.createPrint(System.err);
+
+    /**
+     * This key callback will check if ESC is pressed and will close the window
+     * if it is pressed.
+     */
+    private static GLFWKeyCallback keyCallback = new GLFWKeyCallback() {
+
+        @Override
+        public void invoke(long window, int key, int scancode, int action, int mods) {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+                glfwSetWindowShouldClose(window, true);
+            }
+        }
+    };
+
+    public void createDisplay ()  {
+        /* Set the error callback */
+        glfwSetErrorCallback(errorCallback);
+
+        /* Initialize GLFW */
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Create window
+        long window = glfwCreateWindow(WIDTH, HEIGHT, "Simple example", NULL, NULL);
+        if (window == NULL) {
+            glfwTerminate();
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+
+        /* Center the window on screen */
+        GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        glfwSetWindowPos(window,
+                (vidMode.width() - 640) / 2,
+                (vidMode.height() - 480) / 2
+        );
+
+        // Setup OpenGL context
+        glfwMakeContextCurrent(window);
+        GL.createCapabilities();
+
+        /* Enable vertical synchronization */
+        glfwSwapInterval(1);
+
+        /* Set the key callback */
+        glfwSetKeyCallback(window, keyCallback);
+
+        glfwTerminate();
+    }
+
+    public void updateDisplay () {
+    }
+}
