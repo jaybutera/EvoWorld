073c03fd4999e1b50ac6a8dfac21e388fdce8ae6
Cerbes <cte150030@utdallas.edu>
2017-01-10 17:32:28 -0600

    Comment out current bugs
    Bugs are from physics stuff not currently working in TestCreature

--- src/main/java/TestCreature.java
+++ src/main/java/TestCreature.java
@@ -1,6 +1,6 @@
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.Body;
-import org.jbox2d.p5.*;
+//import org.jbox2d.p5.*;
 import toolbox.Vector3f;
 
 
@@ -9,7 +9,7 @@ public class TestCreature extends Creature {
         super(body, scale);
     }
     
-    Physics phys = new Physics(processing.core.PApplet);
+    //Physics phys = new Physics(processing.core.PApplet);
 
     public void action (float[] a) {
         if(a.length != 2) {
@@ -20,9 +20,9 @@ public class TestCreature extends Creature {
         this.body.applyForceToCenter(new Vec2 (a[0], a[1]));
         
         // apply force at index 0
-        phys.applyForce((Body)this, a[0], -0.5f);
+        //phys.applyForce((Body)this, a[0], -0.5f);
         // apply force at index 1
-        phys.applyForce((Body)this, a[1], 0.5f);
+        //phys.applyForce((Body)this, a[1], 0.5f);
     }
 
     public float[] observation () {
