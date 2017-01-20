5cb1f3385a7fc22130f899b35e49e26cb8f27e0e
Jay Butera <buterajay@gmail.com>
2017-01-19 18:52:21 -0600

    Restructure game root

--- src/main/java/GameRoot.java
+++ src/main/java/GameRoot.java
@@ -1,25 +1,75 @@
+import server.messages.PBCreatureOuterClass;
+import server.messages.PBGameStateOuterClass;
+
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
-abstract public class GameRoot {
-    private ArrayList<FrameListener> frame_listeners;
+public class GameRoot {
+    private ProxyConnector connector;
+    private PetriWorld world;
 
-    public GameRoot () {
-        frame_listeners = new ArrayList<FrameListener>();
+    private boolean connected = false;
+
+    public void initialize () {
+        world = new PetriWorld(1);
+        world.create();
+
+        // Connect to proxy server to update player clients of game state
+        connector = new ProxyConnector("127.0.0.1", 8000);
+
+        try {
+            connected = connector.connect();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    abstract public void run ();
+    public void step () {
+        // Perform physical update
+        world.step();
 
-    protected void start_listeners () {
-        for (FrameListener f : frame_listeners)
-            f.initialize();
+        // Update proxy server
+        if (connected)
+            send();
+
+        for (int j = 0; j < world.d_bodies.length; j++)
+            System.out.println(world.d_bodies[j].serialize().toString());
     }
 
-    protected void update_listeners () {
-        for (FrameListener f : frame_listeners)
-            f.start_frame();
+    public void timed_step () {
+        step();
+
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 
-    public void add_frame_listener(FrameListener f) {
-        frame_listeners.add(f);
+    public Creature[] getCreatures () {
+        return world.d_bodies;
+    }
+
+    private void send () {
+        ArrayList<PBCreatureOuterClass.PBCreature> creatures = new ArrayList<>();
+        for (Creature c : world.d_bodies)
+            creatures.add( c.serialize() );
+
+        try {
+            connector.send(
+                    PBGameStateOuterClass.PBGameState.newBuilder()
+                            .addAllCreatureStat(creatures)
+                            //.addAllFoodStat(null)
+                            .build()
+            );
+        }
+        catch (Exception e) {
+            System.out.println("Failed to send game state to server");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/Visualization.java
+++ src/main/java/Visualization.java
@@ -1,48 +1,29 @@
-/**
- * Created by jaybutera on 1/18/17.
- */
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import processing.core.PApplet;
-import server.messages.PBCreatureOuterClass;
-import server.messages.PBGameStateOuterClass;
 
 public class Visualization extends PApplet {
-    public void setup() {
-        size(500,500);
+    GameRoot root;
 
-        world = new PetriWorld(1);
-        world.create();
+    public void setup() {
+        root = new GameRoot();
+        root.initialize();
+
+        size(500,500);
     }
 
     public void draw(){
-        //ellipse(200,200,50,50);
-
-        // Perform physical update
-        world.step();
+        root.timed_step();
         background(230);
 
-        int x=5;
-        int y=0;
+        root.step();
 
-        for (int j = 0; j < world.d_bodies.length; j++) {
-            System.out.println(world.d_bodies[j].serialize().toString());
-            fill(0,250,0);
-            ellipse(world.d_bodies[j].getPosition().x, -world.d_bodies[j].getPosition().y, 50,50);
-            //ellipse(mouseX, mouseY, 100,100);
-        }
+        Creature[] creatures = root.getCreatures();
 
-        try {
-            TimeUnit.MILLISECONDS.sleep(20);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-            System.exit(-1);
+        // Render
+        for (int j = 0; j < creatures.length; j++) {
+            System.out.println(creatures[j].serialize().toString());
+
+            fill(0, 250, 0);
+            ellipse(creatures[j].getPosition().x, -creatures[j].getPosition().y, 50, 50);
         }
     }
-
-    private PetriWorld world;
 }
