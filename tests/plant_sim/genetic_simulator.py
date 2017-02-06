d3edaa44643c5e06524bd7af0a342626178dbd0f
Jay Butera <buterajay@gmail.com>
2017-02-05 22:09:38 -0600

    Still no convergence

--- tests/plant_sim/genetic_simulator.py
+++ tests/plant_sim/genetic_simulator.py
@@ -3,30 +3,33 @@ from random import random
 
 # Parameters
 pr = .2
-sr = .1
-fr = .2
+sr = 1.8
+fr = 2.2
 water = 2.
 co2 = .5
 oxygen = .8
-light = 4.
+light = 0.
 
 # IC
-atp = .3
-glucose = .6
+atp = 1.
+glucose = 2.
 
 #records = {} # Store plant id and fitness score
 #iteration = 0
 
 def rand():
-    r = random() * .2
-    if r <= 0.:
-        r = .05
+    r = random() * .5
+    #if r <= 0.:
+    #    r = .05
 
     return r
 
 def epoch (seed):
-    plants = [Plant(i, seed.atp + rand(), seed.glucose + rand(), pr, sr, fr) for i in range(100)]
+    plants = [Plant(i+1, seed.atp + rand(), seed.glucose + rand(), pr, sr, fr) \
+            for i in range(1000)]
+    plants.append( seed )
 
+    iteration = 0
     while True:
         # Simulate a timestep and determine which plants died
         plant_stat = []
@@ -41,18 +44,21 @@ def epoch (seed):
         '''
         plants_next = [p for p, s in zip(plants, plant_stat) if s == True]
 
-        print '{0} plants left'.format(len(plants))
+        #print '{0} plants left'.format(len(plants))
 
         # Quit loop if all plants are dead
         if not plants_next:
+            print 'Highest fitness: {0}\n-----------------------'.format(iteration)
             return plants # Return longest surviving plants
 
         # Housekeeping
         plants = plants_next
-        #iteration += 1
+        iteration += 1
 
 
-fit_plant = Plant(0, 1. + rand(), 1. + rand(), pr, sr, fr)
-for i in range(100):
+fit_plant = Plant(0, atp + rand(), glucose + rand(), pr, sr, fr)
+for i in range(10000):
     print 'Epoch {0}'.format(i)
     fit_plant = epoch(fit_plant)[0]
+    print fit_plant.atp_list
+    print fit_plant.glucose_list
--- tests/plant_sim/plant.py
+++ tests/plant_sim/plant.py
@@ -7,10 +7,18 @@ class Plant(object):
         self.starch_rate = sr
         self.fat_rate = fr
 
+        self.atp_list = list()
+        self.glucose_list = list()
+
     # Return false if dead
     def step(self, w,l,c,o):
-        self.atp += -self.atp * self.glucose**2 + o - self.protein_rate - self.fat_rate
-        self.glucose += self.atp * self.glucose**2 - self.glucose + w + c + l - self.starch_rate
+        #self.atp += -self.atp * self.glucose**2 + o - self.protein_rate - self.fat_rate
+        #self.glucose += self.atp * self.glucose**2 - self.glucose + w + c + l - self.starch_rate
+        self.atp += -self.glucose + self.fat_rate#+ o - self.protein_rate - self.fat_rate
+        self.glucose += self.atp + self.starch_rate#+ w + c + l - self.starch_rate
+
+        self.atp_list.append( self.atp )
+        self.glucose_list.append( self.glucose )
 
         if self.atp <= 0. or self.glucose <= 0.:
             return False
