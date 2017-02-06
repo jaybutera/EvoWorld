293600f5ed0813bb8f569f13b954da5d6a3cbf1e
Jay Butera <buterajay@gmail.com>
2017-02-06 11:22:45 -0600

    Add simconnector test object

---
+++ src/main/java/SimConnectorTester.java
@@ -0,0 +1,44 @@
+import gameobjects.Creature;
+
+import java.util.HashMap;
+import java.util.Random;
+
+public class SimConnectorTester {
+    private int num_creatures = 5;
+    private int action_size = 2;
+
+    public SimConnectorTester(String host, int port) {}
+
+    public void connect () {}
+
+    public int[] getIds () {
+        int[] ids = new int[num_creatures];
+
+        for (int i = 0; i < ids.length; i++)
+            ids[i] = i;
+
+        return ids;
+    }
+
+    public void sendObservations (Creature[] creatures) {}
+
+    public HashMap<Integer, float[]> getActions () {
+        HashMap<Integer, float[]> actions = new HashMap<>();
+
+        Random r = new Random();
+
+        // Construct random action dictionary
+        for (int i = 0; i < num_creatures; i++) {
+            float[] a = new float[action_size];
+
+            for (int j = 0; j < action_size; j++)
+                a[j] = r.nextFloat();
+
+            actions.put(i, a);
+        }
+
+        return actions;
+    }
+
+    public void close () {}
+}
