e17896949c05b3d27f689b94c1401f8ff43b80f6
Jay Butera <buterajay@gmail.com>
2017-03-06 00:38:44 -0600

    config setup

---
+++ src/main/java/configurations/Config.java
@@ -0,0 +1,11 @@
+package configurations;
+
+public class Config {
+    public final int food_count;
+    public final int world_size;
+
+    public Config (int fc, int ws) {
+        food_count = fc;
+        world_size = ws;
+    }
+}
---
+++ src/main/java/configurations/readConfig.java
@@ -0,0 +1,42 @@
+package configurations;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+public class readConfig {
+    Config config;
+    InputStream inputStream;
+
+    public Config getPropValues() throws IOException {
+        try {
+            Properties prop = new Properties();
+            String propFileName = "config.properties";
+
+            inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+            }
+
+            Date time = new Date(System.currentTimeMillis());
+
+            // get the property value and print it out
+            config = new Config(
+                    Integer.valueOf(prop.getProperty("food_count")),
+                    Integer.valueOf(prop.getProperty("world_size"))
+            );
+
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            inputStream.close();
+        }
+
+        return config;
+    }
+}
